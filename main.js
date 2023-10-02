ba493d2c1f818d35c85bb7348cf4ed8824dd7f17
Sinan Burak Filiz <57311434+no-bit@users.noreply.github.com>
2023-10-03 00:14:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,16 @@ $(document).ready(function () {
         $(this).addClass("active-filter").siblings().removeClass("active-filter")
     });
 });
+
+const switcher = document.querySelector(".btn");
+switcher.addEventListener("click",function(){
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+    const className = document.body.classList;
+    if (className=="light-theme"){
+        this.textContent="Darks";
+    }else{
+        this.textContent="Lights";
+    }
+    console.log("current class", className);
+});
